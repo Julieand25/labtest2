47d908738d2b28ae908dc7dd85471abbc1db927f
Julieand <julieandmojunggal@gmail.com>
2025-05-05 22:56:49 +0800

    sasve02

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="UTF-8">  <!-- Added charset for better character encoding -->
+  <title>Website Title</title> 
+  
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
